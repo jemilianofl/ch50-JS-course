d654c7adcd8cd48876ffa1dde92eca7f9c3d3469
Emiliano Flores <jemilianofl@gmail.com>
2025-01-29 12:21:14 -0600

    Feat: Functions and Algorithms

---
+++ js-02-functions/src/js/main.js
@@ -0,0 +1,271 @@
+console.log("JS-02 funciones");
+
+/*
+    Las funciones permiten organizar, reutilizar y 
+    estructurar el código. Su propósito principal es:
+    - Modularidad: Dividir un programa en piezas lógicas 
+    más pequeñas y manejables.
+    - Reutilización de código: Reducir la duplicación de 
+    código al reutilizar funciones.
+    - Legibilidad: Hacer que el código sea más claro y fácil de entender.
+    - Mantenimiento: Facilitar actualizaciones o modificaciones 
+    al concentrar la lógica en un solo lugar.
+    - Abstracción: Ocultar detalles internos y expone 
+    solo lo necesario para trabajar con ellas
+
+    Recomendaciones para el uso de las funciones:
+
+    - Nombre descriptivo: Debe indicar claramente lo que hace.
+    - Parámetros: Debe tener cero o más parámetros, separados por coma.
+        Se recomienda no tener más de 3 parámetros.
+    - Seguir el principio SOLID, sobre el principio de responsabilidad única
+    (Single Responsability Principle)
+    - Evitar efectos secundarios: Una función no debe modificar variables externas.
+*/
+
+/* ========================================================================== */
+
+/*
+    https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Functions
+    ----------- Funciones declaradas --------------
+    (function declaration, function statement)
+    Una característica de las funciones declaradas es que tien hoisting.
+    sintaxis:
+    function nombreFuncionCamelCase ( parámetros ){
+        //cuerpo de la función
+        instrucciones;
+    }
+    hoisting: comportamiento en JS que permite a las declaración de variables(var)
+    o funciones se eleven al comienzo de su ámbito antes de que se ejecute el código.
+*/
+
+console.log( saludar() );
+
+function saludar() {
+    console.log("Hola, soy una función declarada");
+}
+
+// Agregar el párametro nombre y mostrar "Hola Serch, soy una función declarada"
+saludarConNombre("Serch");
+function saludarConNombre(nombre) {
+    console.log(`Hola ${nombre}, soy una función declarada`);
+}
+
+/*
+    ----------- Funciones expresadas --------------
+                (function expression)
+    Son funciones declaradas dentro de la asignación de una variable.
+    Estas funciones pueden ser anónimas ( no tienen nombre ).
+    Las funciones expresadas no tienen hoisting, por que no se carga
+    en memoria hasta que se utilice.
+
+    sintaxis:
+        const nombreVariable = function nombreFuncion (parámetros){
+            instrucciones;
+        };
+*/
+//
+
+const sumaDeDosNumeros = function (num1, num2) {
+    return num1 + num2;
+}
+console.log( sumaDeDosNumeros( 3,6 ) ); // 9
+
+let hora = 10;
+let saludarDependiendoHora;
+
+if (hora < 12) {
+    saludarDependiendoHora = function(){
+        return "Buenos dias"
+    };
+} else {
+    saludarDependiendoHora = function(){
+        return "Buenas tardes"
+    };
+}
+
+console.log( saludarDependiendoHora() );
+
+/*
+    ----------- Funciones autoinvocadas -----------------
+                (self-invoking function)
+    Funciones que se autoinvoca, se pueden definir con funciones anónimas
+
+    sintaxis:
+    ( function (párametros) {} )( argumentos );
+
+*/
+
+( function setUp( name ) {
+    console.log(`Hola ${name},  soy una función autoinvocada`);
+}) ("Emiliano");
+
+/*
+--------------- Funciones flecha -----------------------
+                (arrow functions)
+    Funciones similares a las funciones expresadas pero:
+    - No requiere la palabra reservada function
+    - Si tiene una solo instrucción no requiere las llaves {}
+    - Si la instrucción es el mismo retorno, no requiere la palabra return
+    sintaxis:
+        const nombreVariable = (parametros) => instrucción;
+        const nombreVariable = (parametros) => {
+            instrucción;
+            return expresión;
+        }
+*/
+
+// Realizar una función declarada que calcule el área de un rectangulo
+function areaRectangulo(base, altura) {
+    return base * altura;
+}
+
+// Convertir la función anterior a una función expresada
+const rectanguloArea = function (base, altura) {
+    return base * altura;
+}
+
+// Realizar una función flecha que calcule el área de un rectangulo
+const rectangleArea = (height, width) => height * width;
+console.log(`El área del rectángulo: ${rectangleArea(10, 5)} cm2`); // 50
+
+/*
+--------------- Parámetros por defecto ----------------
+                (default parameters)
+    Inicializa un parámetro de  la función, si no se envía el argumento cuando se invoca
+
+*/
+
+const saludarPersona = (nombre) => `Hola ${nombre}, bienvenido`;
+const saludarPersonaConDefaultParameters = (nombre = "Invitado/a") => `Hola ${nombre}, bienvenido`;
+
+console.log( saludarPersona() ); // Hola undefined, bienvenido
+
+console.log( saludarPersonaConDefaultParameters() ); // Hola Invitado/a, 
+
+// Realizar una función que tenga de parámetros el nombre y cohorte
+// Los valores por default de los párametros son nombre=participante cohorte="cohorte de Generation"
+// Retornar el mensaje "Hola nombre, bienvenido/a a la cohorte de Generation"
+// Retornar el mensaje "Hola Dae, bienvenido/a a la cohorte Ch50"
+
+const saludarCohorte = (nombre = "participante", cohorte = "Generation") => `Hola ${nombre}, bienvenido/a a la cohorte de ${cohorte}`;
+console.log( saludarCohorte() );
+console.log( saludarCohorte( "Emiliano", "CH50" ) );
+
+console.log( parseInt("faba") ); // NaN
+console.log( parseInt("faba", 10) ); // NaN
+// base decimal(10 dígitos): 0,1,2,3,4,5,6,7,8,9
+// base hexadecimal(16 dígitos): 0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F
+// base octal(8 dígitos): 0,1,2,3,4,5,6,7
+// base binaria(2 dígitos): 0,1
+console.log( parseInt("faba", 16) ); // 64186 valor decimal de la base hexadecimal
+
+/*
+------------ Funciones de Callback -----------------------
+    Es una función(definida, expresada, arrow, anónima) que se pasa 
+    a otra función como argumento.
+    Se pasa en el argumento como referencia ( sin parentesis).
+ */
+
+ // Arrow function que reciba un parámetro y lo imprima en consola
+const mensajeConsola = mensaje => console.log(mensaje);
+/** 
+    Arrow function que reciba un parámetro y lo imprima con alert
+    @param {string} mensaje mensaje a imprimir
+ */ 
+const mensajeAlert = mensaje => alert(mensaje);
+
+const mensajeTitulo = mensaje => document.getElementById("title").innerText = mensaje;
+
+/**
+  * Función que imprime en consola o alert un mensaje.
+  * @param {string} mensaje mensaje a imprimir
+  * @param {string} opcion "consola" o "alert" el destino a imprimir 
+  */
+const imprimirMensaje = (mensaje = "Hola", opcion = "consola") => {
+    if (opcion === "consola") {
+        mensajeConsola(mensaje);
+    } else if (opcion === "alert") {
+        mensajeAlert(mensaje);
+    } else if (opcion === "titulo") {
+        mensajeTitulo(mensaje);
+    }
+}
+//imprimirMensaje("Hola Lisset", "alert");
+//imprimirMensaje("Hola Serch", "alert");
+imprimirMensaje("Hola Kati");
+imprimirMensaje("Hola Kati", "titulo");
+
+const imprimirMensajeConCallback = (mensaje, funcionCallback) => funcionCallback( mensaje );
+
+imprimirMensajeConCallback("Hola Erick", mensajeConsola);
+imprimirMensajeConCallback("Hola Erick", mensajeTitulo);
+
+// =================== Pase por valor ======================== //
+
+let colorPintura = "rojo";
+
+function cambiarColorPintura(color){
+    console.log(`El color que llega a la función es ${color}`); // rojo
+    color = "azul";
+    console.log(`El color de la pintura que imprime la función es ${color}`); // azul
+}
+
+cambiarColorPintura(colorPintura);
+console.log(`El color de la pintura final es ${colorPintura}`); // rojo
+
+// =================== Pase por referencia ======================== //
+
+const colores = ["rojo", "azul", "verde"];
+function cambiarColorArreglo(arreglo){
+    console.log(`El arreglo que llega a la función es ${arreglo}`); // rojo, azul, verde
+    arreglo[0] = "negro";
+    console.log(`Se imprimen los colores en la función: ${arreglo}`); // negro, azul, verde
+
+}
+
+cambiarColorArreglo( colores );
+console.log(`Los colores finales dentro del arreglo son: ${colores}`) // "negro", "azul", "verde"
+
+// =============================== Método sort() =====================================
+const series = ["Winnie", "Arnold", "avengers" ,"Atreve", "Digimon", "Naruto"];
+console.log( series.sort() ); // ['Arnold', 'Atreve', 'Digimon', 'Naruto', 'Winnie', 'avengers']
+const numeros = [ 122, 2, 1, 55]; 
+console.log( numeros.sort() ); // [1, 122, 2, 55]
+const ordenarNumeros = ( numA, numB) => {
+    if( numA < numB ){
+        return -1; // numA va antes que numB
+    } else if( numA > numB ) {
+        return 1; // numA va después que numB
+    } else { // Los números son iguales
+        return 0
+    }
+}
+console.log( ordenarNumeros( 3,6 ) ); // -1
+console.log( ordenarNumeros( 6,3 ) ); // 1
+console.log( ordenarNumeros( 3,3 ) ); // 0
+
+const numbers = [  1,   10,   3,   100,   5];
+console.log( numbers.sort( ordenarNumeros ) ); //  [1, 3, 5, 10, 100]
+//  [1,10     3,100,5];
+//  [1,3     10,100,5];
+//  [1,3,10,100   ,5 ];
+//  [1,3,10,5,   100 ];
+//  ... después de repetir las iteraciones nuevamente
+//  [1,3,5,10,,100 ];
+
+const ordenarNumerosSimplificados = ( numA, numB) => numA - numB;
+const otrosNumeros = [ 1, 10, 3, 100, 5];
+console.log( otrosNumeros.sort( ordenarNumerosSimplificados ) ); // [1, 3, 5, 10, 100]
+
+const numerosDeLaSuerte = [7, 21, 14, 28, 35];
+console.log( numerosDeLaSuerte.sort( (a,b) => a-b ) );
+console.log( numerosDeLaSuerte.sort( function(a,b){return a-b} ) );
+
+// Ordenar de forma descendente los siguientes números
+const numCh50 = [19, 15, 13, 18, 5];
+
+// 19, 18, 15, 13, 5
+console.log( numCh50.sort( (numA,numB) => numB-numA ) );
+
+
